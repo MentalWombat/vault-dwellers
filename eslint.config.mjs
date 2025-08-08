789a8af2bf31f3019a00196393587628b338f46c
MentalWombat <e.ochneva@gmail.com>
2025-08-08 19:38:02 +0300

    update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,29 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
+import { FlatCompat } from '@eslint/eslintrc';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 const compat = new FlatCompat({
   // import.meta.dirname is available after Node.js v20.11.0
-  baseDirectory: import.meta.dirname,
+  baseDirectory: import.meta.dirname
 });
 
 const eslintConfig = [
   ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+    rules: {
+      '@typescript-eslint/consistent-type-imports': ['error', { fixStyle: 'inline-type-imports' }],
+      'import/no-duplicates': 'error',
+      'import/order': [
+        'error',
+        {
+          named: true,
+          alphabetize: { order: 'asc' },
+          'newlines-between': 'always',
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index']
+        }
+      ]
+    }
   }),
-  eslintConfigPrettier,
+  eslintConfigPrettier
 ];
 
 export default eslintConfig;
